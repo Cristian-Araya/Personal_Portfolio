543f74af6d3757d8b0de34d3324348e0626ff28c
Cristian-Araya <Arayacristian1398@gmail.com>
2021-03-18 11:38:09 -0300

    commit
    Se crea el proyecto utilizando create-react-app y se eliminan archivos innecesarios

---
+++ my-portfolio/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <span class="navbar-brand mb-0 h1">Cristian Araya</span>
+                    
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ my-portfolio/src/Pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar.jsx'
+import Hero from '../Components/Hero/Hero.jsx'
+
+const MainPage = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Hero/>
+        </div>
+    )
+}
+
+export default MainPage
